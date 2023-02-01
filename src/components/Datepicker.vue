6ec7ca9f38a82aa698e8a7695e0596a8cc826b8d
Jacky Chia <yangjie19991@gmail.com>
2023-02-01 14:44:52 +0800

    datepicker

---
+++ src/components/Datepicker.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <Datepicker inputFormat="yyyy-MMM-dd" locale="my" :enableTimePicker="false" v-model="date"  @closed="onchangeDate" :format="datepickerFormat" ></Datepicker>
+        <p>{{ date }}</p>
+    </div>
+</template>
+  
+<script>
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+    components: {
+        Datepicker,
+    },
+    data() {
+        return {
+            value: true,
+            date: new Date(2020, 1, 1),
+            pickerConfig: {
+                value: '2020/10/22'
+            },
+            defaultDate: '2018-12-04',
+            datepickerFormat:'yyyy-MMM-dd'
+        }
+    },
+    methods: {
+
+        otherChangeHandler: function (eventData, yourArg1, yourArg2) {
+            console.log(eventData)
+            // -> [{data: {HH:..., mm:... }}]
+            console.log(yourArg1)
+            // -> 'foo'
+            console.log(yourArg2)
+            // -> 'bar'
+        },
+        changeHandler(eventData) {
+            console.log(eventData)
+            // -> {data: {HH:..., mm:... }}
+        },
+        getSelectedDate() {
+            console.log(this.$refs.datepicker1.value);
+            //this.$emit("selected", selectedDate)
+        },
+        onchangeDate:function(){
+            console.log(this.date);
+            this.$emit('select-date-from-datepicker',this.date)
+        }
+    }
+}
+</script>
+  
+<style src="@vueform/toggle/themes/default.css">
+
+</style>
